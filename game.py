2dc6a82c63a422e80002f2dd919446c86d7107d5
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-06-16 19:13:30 +0300

    Add docstrings to game.py
    Added docstrings that define what the method does in the Game class in game.py

--- game.py
+++ game.py
@@ -8,8 +8,10 @@ from board import Board
 
 
 class Game:
+	"""The class that represents the whole application."""
 
 	def __init__(self):
+		"""Initialize pygame, the screen and the board."""
 		pg.init()
 		self.screen = pg.display.set_mode(SCREEN_PROPERTIES)
 		pg.display.set_caption(WINDOW_TITLE)
@@ -18,16 +20,19 @@ class Game:
 		self.board = Board()
 
 	def start(self):
+		"""Start the main loop of the game."""
 		while True:
 			self.poll_events()
 			self.update_screen()
 
 	def poll_events(self):
+		"""Check events regarding the pygame window."""
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
 				sysexit(1)
 
 	def update_screen(self):
+		"""Draw/render objects to the screen."""
 		self.screen.fill(BACKGROUND_COLOR)
 
 		self.board.render(self.screen)
