984940081da12f6e9a93dd42712df0fa07078b35
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-06-18 21:37:39 +0300

    Add directory settings to constants
    Added constant directory names that are initialized when the program starts. The most important is ROOT_DIR. It provides a way to navigate through other files. The other is PIECE_DIR, which contains the images for the pieces.

--- constants.py
+++ constants.py
@@ -4,6 +4,12 @@ This module defines constants and enumerations regarding the whole application.
 
 from enum import Enum
 
+from pathlib import Path
+
+# Settings
+ROOT_DIR = Path(__file__).resolve().parent
+PIECE_DIR = (ROOT_DIR / 'assets') / 'pieces'
+
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -16,7 +22,7 @@ BACKGROUND_COLOR = (192, 192, 192)
 SQUARE_FONT_COLOR = (32, 30, 31)
 
 
-# Functionality constants/enums
+# Piece-related stuff
 class PieceColor(Enum):
 	"""The color of a piece."""
 	LIGHT = 1
