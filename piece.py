614124b125dfbb21a53778194d48110880e47781
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-06-18 21:43:13 +0300

    Piece rendering works properly
    Pieces render as they should, in their correct squares. White pieces render white, black pieces render black.

--- piece.py
+++ piece.py
@@ -1,26 +1,60 @@
 from typing import Sequence
-
 from abc import abstractmethod
 
+import pygame as pg
+
 from square import Square
-from constants import PieceColor
+from constants import PieceColor, SQUARE_FONT_COLOR, PIECE_DIR
 from base import BaseDrawable
+
+
+pg.font.init()  # initialize pygame's font module
 
 
 class BasePiece(BaseDrawable):
 	"""The base piece class. Represents a piece on the chessboard."""
-	points: int = None  # how much the piece is worth
-	notation: str = None  # how the piece is represented in chess notation
+	# Fonts
+	RENDER_FONT_PROPERTIES = ('monospace', 22)
+	RENDER_FONT = pg.font.SysFont(*RENDER_FONT_PROPERTIES)
+
+	# Dicts
+	PIECE_DICT = {
+		'Pawn': 'p', 'Knight': 'n', 'Bishop': 'b',
+		'Rook': 'r', 'Queen': 'q', 'King': 'k'
+	}
+
+	points: int  # how much the piece is worth
+	notation: str  # how the piece is represented in chess notation
 
 	def __init__(self, color: PieceColor, square: Square):
 		"""Initialize a piece with a color and square."""
 		self.color = color
 		self.square = square
+
+		# Load image and get its rect
+		imagename = f"{'b' if self.color == PieceColor.DARK else 'w'}" \
+			f"{BasePiece.PIECE_DICT[self.__class__.__name__]}.png"
+		self.image = pg.image.load(PIECE_DIR / imagename).convert_alpha()
+
+		self.rect = self.image.get_rect()
+
+	def update_pos(self, surface):
+		square_pos = self.square.get_pos(surface)
+		self.rect.centerx = square_pos[0] + Square.SQUARE_SIZE // 2
+		self.rect.centery = square_pos[1] + Square.SQUARE_SIZE // 2
 	
 	@abstractmethod
-	def get_movable_squares(self, squares: Sequence[Square]) -> Sequence[Square]:
+	def get_possible_moves(self, squares: Sequence[Square]) -> Sequence[Square]:
 		"""Get the valid squares the piece can move to."""
 		raise NotImplemented
+
+	@abstractmethod
+	def render(self, surface) -> None:
+		"""
+		Abstract method for rendering the piece to the screen. Provides a default implementation that prints the
+		letter of the piece to its square on the screen.
+		"""
+		surface.blit(self.image, self.rect)
 
 	def __str__(self):
 		colorname = 'Light' if self.color == PieceColor.LIGHT else 'Dark'
@@ -33,16 +67,17 @@ class BasePiece(BaseDrawable):
 class Pawn(BasePiece):
 	"""Represents a pawn on the chessboard."""
 	points = 1
+	notation = 'P'
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.has_moved = False
 
-	def get_movable_squares(self, squares):
+	def get_possible_moves(self, squares):
 		pass
 
 	def render(self, surface):
-		pass
+		super().render(surface)
 
 
 class Bishop(BasePiece):
@@ -53,11 +88,11 @@ class Bishop(BasePiece):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
-	def get_movable_squares(self, squares):
+	def get_possible_moves(self, squares):
 		pass
 
 	def render(self, surface):
-		pass
+		super().render(surface)
 
 
 class Knight(BasePiece):
@@ -68,11 +103,11 @@ class Knight(BasePiece):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
-	def get_movable_squares(self, squares):
+	def get_possible_moves(self, squares):
 		pass
 
 	def render(self, surface):
-		pass
+		super().render(surface)
 
 
 class Rook(BasePiece):
@@ -83,11 +118,11 @@ class Rook(BasePiece):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
-	def get_movable_squares(self, squares):
+	def get_possible_moves(self, squares):
 		pass
 
 	def render(self, surface):
-		pass
+		super().render(surface)
 
 
 class Queen(BasePiece):
@@ -98,11 +133,11 @@ class Queen(BasePiece):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
-	def get_movable_squares(self, squares):
+	def get_possible_moves(self, squares):
 		pass
 
 	def render(self, surface):
-		pass
+		super().render(surface)
 
 
 class King(BasePiece):
@@ -113,8 +148,8 @@ class King(BasePiece):
 		self._has_moved = False
 		super().__init__(*args, **kwargs)
 
-	def get_movable_squares(self, squares):
+	def get_possible_moves(self, squares):
 		pass
 
 	def render(self, surface):
-		pass
+		super().render(surface)
