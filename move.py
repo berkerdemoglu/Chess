f4f09159f5d89e1a96c29eff19b2a7c0c718633e
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-06-19 13:37:37 +0300

    Prevent same color capturing same color
    Added a simple if statement that renders a move invalid if the moving piece and the occupying piece are the same color.

--- move.py
+++ move.py
@@ -9,21 +9,32 @@ from piece import BasePiece, King
 class Move:
 
 	def __init__(self, to: Square, moving_piece: BasePiece, occupying_piece: BasePiece):
+		"""
+		Initialize a move with a the square to move to, the moving
+		piece and the occupant of the square the piece is moving to.
+		"""
 		self.to = to
 		self.moving_piece = moving_piece
 		self.occupying_piece = occupying_piece
 
 	def _check_capture(self, pieces_list: List[BasePiece]):
+		"""Check if a piece is being captured."""
 		if self.moving_piece != self.occupying_piece is not None:
 			pieces_list.remove(self.occupying_piece)
 
 	def is_valid(self) -> bool:
+		"""Check the validity of the move."""
+		if self.occupying_piece is not None:
+			if self.moving_piece.color == self.occupying_piece.color:
+				return False
+
 		if type(self.occupying_piece) == King:
 			return False
 
 		return True
 
 	def make_move(self, surface: Surface, pieces_list: List[BasePiece]) -> None:
+		"""Make the move on the board, if it is valid."""
 		if self.is_valid():
 			self._check_capture(pieces_list)
 			self.moving_piece.square = self.to
