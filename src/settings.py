ed7a3a859a169ad493f24b176c4a7fd767b17594
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-06-27 21:20:47 +0300

    Move all source files to src
    Moved all source files to a new directory called 'src'.

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,9 @@ This module defines constants/settings for the whole application.
 
 from pathlib import Path
 
-# Settings
-ROOT_DIR = Path(__file__).resolve().parent
+# Path constants
+SRC_DIR = Path(__file__).resolve().parent
+ROOT_DIR = SRC_DIR.parent
+
 ASSETS_DIR = ROOT_DIR / 'assets'
 PIECE_DIR = ASSETS_DIR / 'pieces'
