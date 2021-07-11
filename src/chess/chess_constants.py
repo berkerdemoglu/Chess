4391298e8a6bf2c5b4319458ff88ea16cf269790
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-07-11 20:50:32 +0300

    Create a Direction enum
    Created a 'Direction' enum. Its members have values for square index differences from white's perspective. This is then used in move generation.

--- src/chess/chess_constants.py
+++ src/chess/chess_constants.py
@@ -9,3 +9,14 @@ class ChessColor(Enum):
 	@classmethod
 	def negate(cls, color: 'ChessColor'):
 		return cls(-color.value)
+
+
+class Direction(Enum):
+	"""
+	A direction on the board from white's perspective represented by 
+	square index differences. For black, just multiply them by -1.
+	"""
+	FORWARD = -8
+	BACK = 8
+	RIGHT = 1
+	LEFT = -1
